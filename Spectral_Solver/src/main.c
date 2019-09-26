f016825d04f03abed7db7ce91f2617dfaf0af7a2
Enda Carroll <ecarroll@chirp.ucd.ie>
2019-09-26 18:17:37 +0100

    Created solver folder, makefile and main.c

---
+++ Spectral_Solver/src/main.c
@@ -0,0 +1,18 @@
+// Enda Carroll
+// Sept 2019
+// Main file for calling the pseudospectral solver for the 1D Burgers equation
+
+// ---------------------------------------------------------------------
+//  Standard Libraries and Headers
+// ---------------------------------------------------------------------
+#include <stdlib.h>
+#include <stdio.h>
+#include <maths.h>
+#include <time.h>
+#include <complex.h>
+#include <fftw3.h>
+
+
+// ---------------------------------------------------------------------
+//  User Libraries and Headers
+// ---------------------------------------------------------------------
